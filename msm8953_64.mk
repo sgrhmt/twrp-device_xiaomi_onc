f9b93595bb3998c113531afa337d3515f7e99124
Muhammed Siju <msiju@codeaurora.org>
2020-09-24 15:06:49 +0530

    Telephony: Disable advance network scan for legacy targets
    - Set persist.vendor.radio.enableadvancedscan to false
    
    Change-Id: Id01e7a545e89ff9ab9fb659aec1fbae527c40f71
    CRs-Fixed: 2780132

--- msm8953_64.mk
+++ msm8953_64.mk
@@ -423,6 +423,10 @@ ifeq ($(strip $(TARGET_KERNEL_VERSION)), 3.18)
     PRODUCT_PACKAGES += vendor-extra-libs
 endif
 
+#vendor prop to disable advanced network scanning
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.enableadvancedscan=false
+
 # Enable telephpony ims feature
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
